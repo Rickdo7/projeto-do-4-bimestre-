131979cb9247ace96c2b31122ab1e127ef2e56ba
unknown <arthurruvio486@gmail.com>
2025-11-19 14:56:08 -0300

    at

--- app/index.js
+++ app/index.js
@@ -1,40 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, ImageBackground, Platform } from 'react-native';
 import banner1 from "../assets/banner1.png";
-import * as SplashScreen from 'expo-splash-screen';
-import useBebasFont from "../hooks/useFont/index.js";
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function App() {
-  const fontLoaded = useRighteousFont();
-
-  if (!fontLoaded) {
-    return (
-        <View style={styles.loadingContainer}>
-            <ActivityIndicator size={30} color="#000" />
-        </View>
-    );
-}
 
 
 return (
-  <SafeAreaView>
+    <SafeAreaView>
   <View style={styles.container}>
-    
-    <View style={styles.main}>
+    <View style={styles.fundo}>
 
-    
     <View style={styles.banner1}>
+    <View style={styles.main}>
     
       <Image source={banner1} style={styles.imgb1} />
     
-      <Text style={styles.titulo}> SINTA A EMOÇÃO DO FUTEBOL!! </Text>
-    
+      
+      <Text style={styles.titulo}>  </Text>
+  
+    </View>
+    <View sub>
+
+
     </View>
     
-    </View>
     
+    </View>
+    </View>
     <StatusBar style="auto" />
     </View>
     </SafeAreaView>
@@ -46,38 +41,42 @@ return (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#90ff17',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 10,
-    margin: 0,
-
-  },
-  banner1: {
-    marginTop: -370,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    height: 400,
-    width: 600,
+    
     
   },
-  imgb1: {
-
-    height: 400,
-    width: 600
-  },
   titulo: {
-    fontSize:Platform.OS ==="web"? 48 : 36,
+    fontSize:Platform.OS ==="web"? 48 : 26,
     marginBottom:20,
     textTransform:"uppercase",
     fontWeight:Platform.OS === "web"? "bold" :null
+    
+  },
+  banner1: {
+    marginTop: 250,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+   
+  },
+  imgb1: {
+    marginTop:370,
+    borderRadius:30,
+    height: 7 00,
+    width: 970,
   },
   main:{
     flexDirection:Platform.OS ==="web"? "row" :"column",
     gap:20,
     alignItems:"center",
   },
-
-
+  tela:{
+    backgroundColor:"white",
+    height:10,
+  },
+ 
+  
+  
 });
